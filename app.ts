71991262fcb313fe537c8441ed5f454356615f56
Muhammad Momin <160387789+MuhammadMomin095@users.noreply.github.com>
2024-03-26 00:36:43 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,18 @@
+function divideAndRemainder(
+
+    dividend:number,
+     divisor:number
+
+
+     ):{ quotient: number; remainder: number }{
+
+
+    let quotient = Math.floor(dividend / divisor);
+    let remainder = dividend % divisor
+
+    
+    return { quotient, remainder};
+}
+
+
+console.log(divideAndRemainder(10, 3));
